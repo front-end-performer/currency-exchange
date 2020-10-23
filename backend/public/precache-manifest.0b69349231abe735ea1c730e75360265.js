400c5ddc1f089aa91835bf9afd6299d678a8f73d
Oleksii Sodolinskyi <admin@Oleksiis-MBP.fritz.box>
2020-10-23 19:46:16 +0200

    first commit

---
+++ backend/public/precache-manifest.0b69349231abe735ea1c730e75360265.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d31492a761d62a2cc03018df8e2c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25cb5264dc9ad72db03",
+    "url": "/static/css/5.6481878c.chunk.css"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/css/main.c8dd1876.chunk.css"
+  },
+  {
+    "revision": "01249d7719264b213673",
+    "url": "/static/js/0.2f5dc2e5.chunk.js"
+  },
+  {
+    "revision": "a8a90041304c354727b1",
+    "url": "/static/js/3.2b5b14c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/3.2b5b14c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb51000c3afe49a0dfcf",
+    "url": "/static/js/4.212edbb0.chunk.js"
+  },
+  {
+    "revision": "b25cb5264dc9ad72db03",
+    "url": "/static/js/5.2bc43e44.chunk.js"
+  },
+  {
+    "revision": "f2a40e095ebc7d90d92b",
+    "url": "/static/js/6.f39796fb.chunk.js"
+  },
+  {
+    "revision": "3cfd5f380d85874231f9",
+    "url": "/static/js/7.f4982ebe.chunk.js"
+  },
+  {
+    "revision": "f999777fcf1800337f36",
+    "url": "/static/js/main.2e384de5.chunk.js"
+  },
+  {
+    "revision": "8c30f4fab154b1f42569",
+    "url": "/static/js/runtime-main.ca3e4701.js"
+  }
+]);
